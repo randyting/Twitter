c0456e48cef2300af5f70c391c02b8b07a1ed6da
Randy Ting <randy@randyting.com>
2015-09-30 01:28:05 -0700

    Initial commit.

---
+++ Twitter/API/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  Twitter
+//
+//  Created by Randy Ting on 9/29/15.
+//  Copyright © 2015 Randy Ting. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1RequestOperationManager {
+
+}
---
+++ Twitter/Models/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  Twitter
+//
+//  Created by Randy Ting on 9/30/15.
+//  Copyright © 2015 Randy Ting. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+
+}
